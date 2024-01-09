b6b006bff6b18dad3e590e7b0b68dae9a5c9245c
Michael Tsang <michael.tsang@jnction.co.uk>
2024-01-09 10:41:23 +0000

    Hounslow and Kingston loops

--- src/gtfs/repository/ScheduleBuilder.ts
+++ src/gtfs/repository/ScheduleBuilder.ts
@@ -165,22 +165,130 @@ export class ScheduleBuilder {
   public fillStopHeadsigns(atoc_code : string | null, stops : StopTime[], destination_name : string | null) : void {
     if (stops.length === 0) return;
 
+    const destination_code = stops[stops.length - 1].stop_id.substring(0, 3);
+    function findCallingIndex(stop_code : string) : number | null {
+      const result = stops.findIndex(stop => stop.stop_id.substring(0, 3) === stop_code);
+      return result === -1 ? null : result;
+    }
     // Sutton (via Mitcham Junction) / Sutton (via Wimbledon)
     if (stops[stops.length - 1].stop_id.substring(0, 3) === 'SUO' && atoc_code === 'TL') {
-      const index = stops.findIndex(stop => stop.stop_id.substring(0, 3) === 'STE');
-      const wimbledon = stops.find(stop => stop.stop_id.substring(0, 3) === 'WIM');
-      const mitcham = stops.find(stop => stop.stop_id.substring(0, 3) === 'MIJ');
-      if (index !== -1) {
+      const index = findCallingIndex('STE');
+      const wimbledon = findCallingIndex('WIM');
+      const mitcham = findCallingIndex('MIJ');
+      if (index !== null) {
         for (let i = 0; i <= index; ++i) {
-          stops[i].stop_headsign = wimbledon ? 'Sutton (via Wimbledon)' : mitcham ? 'Sutton (via Mitcham Junction)' : null;
+          stops[i].stop_headsign = wimbledon !== null ? 'Sutton (via Wimbledon)' : mitcham !== null ? 'Sutton (via Mitcham Junction)' : null;
         }
       }
     }
 
     if (stops[0].stop_id.substring(0, 3) === 'SUO' && atoc_code === 'TL') {
-      const wimbledon = stops.find(stop => stop.stop_id.substring(0, 3) === 'WIM');
-      const mitcham = stops.find(stop => stop.stop_id.substring(0, 3) === 'MIJ');
-      stops[0].stop_headsign = wimbledon ? `${destination_name} (via Wimbledon)` : mitcham ? `${destination_name} (via Mitcham Junction)` : null;
+      const wimbledon = findCallingIndex('WIM');
+      const mitcham = findCallingIndex('MIJ');
+      stops[0].stop_headsign = wimbledon !== null ? `${destination_name} (via Wimbledon)` : mitcham !== null ? `${destination_name} (via Mitcham Junction)` : null;
+    }
+
+    // https://www.railforums.co.uk/threads/services-advertised-as-terminating-at-penultimate-station.252431/post-6453655
+    if (atoc_code === 'SW') {
+      const strawberry_hill = findCallingIndex('STW');
+      const twickenham = findCallingIndex('TWI');
+      const richmond = findCallingIndex('RMD');
+      const hounslow = findCallingIndex('HOU');
+      const chiswick = findCallingIndex('CHK');
+      const kingston = findCallingIndex('KNG');
+      const teddington = findCallingIndex('TED');
+      const staines = findCallingIndex('SNS');
+      const wimbledon = findCallingIndex('WIM');
+      const barnes_bridge = findCallingIndex('BNI');
+      const brentford = findCallingIndex('BFD');
+      const mortlake = findCallingIndex('MTL');
+      const barnes = findCallingIndex('BNS');
+      const addlestone = findCallingIndex('ASN');
+      for (let i = 0; i < stops.length; ++i) {
+        const stop = stops[i];
+        const stop_code = stop.stop_id.substring(0, 3);
+
+        // Kingston loop clockwise
+        if (['WAT', 'VXH', 'CLJ'].includes(stop_code) && wimbledon !== null && strawberry_hill !== null && i < wimbledon && wimbledon < strawberry_hill && staines === null) {
+          stop.stop_headsign ??= 'Strawberry Hill (via Wimbledon)';
+        }
+        if (kingston !== null && richmond !== null && kingston < richmond && i < kingston) {
+          stop.stop_headsign ??= 'Richmond';
+        }
+        if (kingston !== null && chiswick !== null && kingston < chiswick && i < kingston) {
+          stop.stop_headsign ??= 'Chiswick';
+        }
+
+        // Kingston loop anti-clockwise
+        if (['WAT', 'VXH', 'QRB', 'CLJ'].includes(stop_code) && strawberry_hill !== null && twickenham !== null && i < twickenham && twickenham < strawberry_hill) {
+          const false_destination = teddington !== null ? 'Teddington' : destination_name;
+          if (richmond !== null) {
+            stop.stop_headsign ??= `${false_destination} (via Richmond)`;
+          }
+          if (hounslow !== null) {
+            stop.stop_headsign ??= `${false_destination} (via Hounslow)`;
+          }
+        }
+        if (wimbledon !== null && twickenham !== null && i <= twickenham && twickenham < wimbledon) {
+          stop.stop_headsign ??= 'Wimbledon';
+        }
+
+        // Kingston loop between Kingston and Strawberry Hill
+        if (['KNG', 'HMW', 'TED', 'STW', 'SHP', 'UPH', 'SUU', 'KMP', 'HMP', 'FLW'].includes(stop_code) && ['WAT', 'CLJ'].includes(destination_code)) {
+          if (richmond !== null && i < richmond) {
+            stop.stop_headsign ??= `${destination_name} (via Richmond)`;
+          }
+          if (hounslow !== null && i < hounslow) {
+            stop.stop_headsign ??= `${destination_name} (via Hounslow)`;
+          }
+          if (wimbledon !== null && i < wimbledon) {
+            stop.stop_headsign ??= `${destination_name} (via Wimbledon)`;
+          }
+        }
+
+        // Hounslow loop clockwise
+        if (hounslow !== null && richmond !== null && richmond < hounslow && i < richmond && ['WAT', 'VXH', 'QRB', 'CLJ', 'WNT', 'PUT', 'BNS'].includes(stop_code)) {
+          stop.stop_headsign ??= 'Hounslow (via Richmond)';
+        }
+        if (twickenham !== null && chiswick !== null && twickenham < chiswick && i <= twickenham) {
+          stop.stop_headsign ??= 'Chiswick';
+        }
+        if (stop_code === 'WTN' && barnes_bridge !== null && i < barnes_bridge) {
+          stop.stop_headsign ??= 'Barnes Bridge';
+        }
+
+        // Hounslow loop anti-clockwise
+        if (hounslow !== null && brentford !== null && brentford < hounslow && i < brentford && staines === null) {
+          if (['WAT', 'VXH', 'QRB', 'CLJ', 'WNT', 'PUT', 'BNS'].includes(stop_code)) {
+            stop.stop_headsign ??= 'Hounslow (via Brentford)';
+          } else {
+            stop.stop_headsign ??= 'Hounslow';
+          }
+        }
+        if (hounslow !== null && mortlake !== null && i < hounslow && hounslow < mortlake) {
+          stop.stop_headsign ??= 'Mortlake';
+        }
+
+        if (stop_code === 'HOU' && ['WAT', 'CLJ'].includes(destination_code)) {
+          if (brentford !== null && i < brentford) {
+            stop.stop_headsign ??= `${destination_name} (via Brentford)`;
+          }
+          if (richmond !== null && i < richmond) {
+            stop.stop_headsign ??= `${destination_name} (via Richmond)`;
+          }
+        }
+
+        // Weybridge (via Hounslow) service
+        if (i === 0 && stop_code === 'WAT' && addlestone !== null) {
+          stop.stop_headsign ??= 'Addlestone';
+        }
+        if (['WYB', 'WOK'].includes(destination_code) && staines !== null && ['VXH', 'QRB', 'CLJ'].includes(stop_code)) {
+          stop.stop_headsign ??= `${destination_name} (via Staines)`;
+        }
+        if (addlestone !== null && barnes !== null && addlestone < barnes && i < addlestone) {
+          stop.stop_headsign ??= 'Barnes';
+        }
+      }
     }
   }
 }
