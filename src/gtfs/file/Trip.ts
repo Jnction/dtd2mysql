dc5e9e5754e3ebc71cad42c4ba69d92e5ef0bea3
Michael Tsang <michael.tsang@jnction.co.uk>
2024-02-20 11:51:29 +0000

    add Shape GTFS definition

--- src/gtfs/file/Trip.ts
+++ src/gtfs/file/Trip.ts
@@ -7,6 +7,7 @@ export interface Trip {
   trip_headsign: string | null;
   trip_short_name: RSID;
   direction_id: 0 | 1;
+  shape_id?: number;
   wheelchair_accessible: 0 | 1 | 2;
   bikes_allowed: 0 | 1 | 2;
 }
