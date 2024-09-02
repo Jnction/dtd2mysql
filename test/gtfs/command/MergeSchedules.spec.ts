8e0f76f2da50625b9038ace4546b73301f44630c
Michael Tsang <michael.tsang@jnction.co.uk>
2024-09-02 10:22:11 +0100

    fix test

--- test/gtfs/command/MergeSchedules.spec.ts
+++ test/gtfs/command/MergeSchedules.spec.ts
@@ -1,4 +1,5 @@
 import moment = require("moment");
+import {AtcoCode, CRS, TIPLOC} from '../../../src/gtfs/file/Stop';
 import {STP, TUID} from "../../../src/gtfs/native/OverlayRecord";
 import {Days, ScheduleCalendar} from "../../../src/gtfs/native/ScheduleCalendar";
 import {StopTime} from "../../../src/gtfs/file/StopTime";
@@ -7,13 +8,44 @@ import {RouteType} from "../../../src/gtfs/file/Route";
 
 const ALL_DAYS: Days = { 0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1 };
 
+const DEFAULT_STOP_TIMES : StopTime[] = [
+  {
+    trip_id: 'LN1234',
+    arrival_time: '12:30',
+    departure_time: '12:30',
+    stop_id: '9100XXXXXX1',
+    stop_code: 'XXX',
+    tiploc_code: 'XXXXXX',
+    stop_sequence: 0,
+    stop_headsign: null,
+    pickup_type: 0,
+    drop_off_type: 0,
+    shape_dist_traveled: null,
+    timepoint: 1,
+  },
+  {
+    trip_id: 'LN1234',
+    arrival_time: '13:00',
+    departure_time: '13:00',
+    stop_id: '9100YYYYYY1',
+    stop_code: 'YYY',
+    tiploc_code: 'YYYYYY',
+    stop_sequence: 1,
+    stop_headsign: null,
+    pickup_type: 0,
+    drop_off_type: 0,
+    shape_dist_traveled: null,
+    timepoint: 1,
+  },
+]
+
 export function schedule(id: number,
                          tuid: TUID,
                          from: string,
                          to: string,
                          stp: STP = STP.Overlay,
                          days: Days = ALL_DAYS,
-                         stops: StopTime[] = []): Schedule {
+                         stops: StopTime[] = DEFAULT_STOP_TIMES): Schedule {
 
   return new Schedule(
     id,
