299d72a9199885e112235a34882d268b5f6a5858
Michael Tsang <michael.tsang@jnction.co.uk>
2024-10-25 14:19:23 +0100

    try to identify night Overground services on BST / GMT change

--- src/gtfs/command/AddLateNightServices.ts
+++ src/gtfs/command/AddLateNightServices.ts
@@ -1,5 +1,5 @@
 import {Schedule} from "../native/Schedule";
-import {IdGenerator} from "../native/OverlayRecord";
+import {IdGenerator, STP} from "../native/OverlayRecord";
 
 /**
  * Loop through every schedule and replace any early morning services with a copy on the previous day.
@@ -18,7 +18,16 @@ export function addLateNightServices(schedules: Schedule[], idGenerator: IdGener
     // some trains start at an unadvertised stop - we assume that the departure hour is at the first public call
     const departureHour = parseInt(schedule.stopTimes.find(s => s.departure_time != null)!.departure_time!.substr(0, 2), 10);
 
-    if (departureHour <= 1) {
+    let isAnStpOvergroundServiceDepartingAtTheRepeatedHourAtAutumnClockChange =
+        // the service is an STP service operated by London Overground    
+        schedule.operator === 'LO' && schedule.stp === STP.New
+        // which is only active for one day
+        && schedule.calendar.runsFrom.isSame(schedule.calendar.runsTo)
+        // and that day is the last Sunday of October
+        && schedule.calendar.runsFrom.month() === 10 - 1 && schedule.calendar.runsFrom.date() >= 25 && schedule.calendar.runsFrom.weekday() === 0
+        // and it departs between 01:00 and 01:59
+        && departureHour === 1
+    if (departureHour <= 1 && !isAnStpOvergroundServiceDepartingAtTheRepeatedHourAtAutumnClockChange) {
       const newSchedule = schedule.clone(schedule.calendar.shiftBackward(), idGenerator.next().value);
 
       for (const stop of newSchedule.stopTimes) {
