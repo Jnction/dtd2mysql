679d3b28352b27a07101ab20e32a471e979510da
Michael Tsang <michael.tsang@jnction.co.uk>
2025-06-19 10:23:24 +0100

    fix services of unknown TOC generating duplicate route ID

--- src/gtfs/native/Schedule.ts
+++ src/gtfs/native/Schedule.ts
@@ -208,7 +208,7 @@ export class Schedule implements OverlayRecord {
       "ZZ": {name: "Other operator", colour: null}
     };
 
-    return {route_id : prefix, ...tocData[prefix ?? ''] ?? {name : rsid, colour : null}};
+    return {route_id : prefix, ...tocData[prefix ?? ''] ?? {name : prefix, colour : null}};
   }
 
   /**
